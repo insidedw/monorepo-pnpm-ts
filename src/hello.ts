5990027b9ba286ee41e011b67ed61c427d2177af
daewoongkim <insidedw@gmail.com>
2024-07-15 22:58:23 +0900

    first commit

---
+++ src/hello.ts
@@ -0,0 +1,1 @@
+export const getLength = (a: string) => a.length
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import {getLength} from "@cal/hello";
+import {add} from '@S_shared/utils'
+
+const a = add(3, 5)
+console.log('a', a)
+
+
+const b = getLength('dddd')
+console.log('b', b)
